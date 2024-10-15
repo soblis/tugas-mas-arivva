7df3fba962ea29f95bbc2566dd9dd8de18e38218
soblis <alexsander6498@gmail.com>
2024-10-15 22:53:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+    var ctx = document.getElementById('barChart').getContext('2d');
+    var barChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+            datasets: [{
+                label: 'Sales',
+                data: [120, 190, 300, 500, 200, 300, 450],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+};
